2921d72bbcc81052dcc94c47fda7a7181ede6326
CrisSsSs10 <crismajoss10@gmail.com>
2025-05-01 16:04:04 -0400

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App (){
+return <div className="container">
+
+
+<h1 online timer></h1>
+
+<div className="weather"></div>
+<div className="logo"></div>
+<img src=""
+<h2 firefox </h2>
+<div className="search"></div>
+<div className="icons"></div>
+<div className="outer"></div>
+<div className="activity"></div>
+<h4>recent activiry</h4>
+<div/>
+<div> className="settings" </div>
+}
+
+export default App
